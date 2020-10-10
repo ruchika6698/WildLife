c2e6bd1a7bbc71a85cc26c3f7b5b427ed31a2c41
ruchika6698 <ruchika6698@gmail.com>
2020-10-10 14:26:06 +0530

    Added:Media queries and css for donation form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from './Components/Navigation'
+import Donationform from './Components/Donationform'
 
 function App() {
   return (
     <div>
       <div>
         <Navigation/>
+        <Donationform/>
       </div>
     </div>
   );

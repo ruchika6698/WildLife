34aaedb73f7bc1704aaeef12f88404b381cdf356
ruchika6698 <ruchika6698@gmail.com>
2020-10-12 15:20:37 +0530

    Added:Animal Galary component

---
+++ src/Components/Galary.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../SCSS/Galary.scss";
+import Button from "react-bootstrap/Button";
+
+export default function Galary() {
+  return (
+    <div className="Galary">
+     <div>
+        <div className="Galarytext">GALLERY</div>
+        <div className="Savedanimal">Saved Animals</div>
+        <br/>
+        <div className="Textinfo">In our gallery, you can take a look at the photos of wild cats and other</div>
+        <div className="Textinfo">animals that we’ve rescued and helped since our first day.</div>
+        <br/><br/>
+        <div className="Galary1">
+            <div className="Animal1"><Button className="Zoom">Zoom</Button></div>
+            <div className="Animal2"><Button className="Zoom">Zoom</Button></div>
+            <div className="Animal3"><Button className="Zoom">Zoom</Button></div>
+            <div className="Animal4"><Button className="Zoom">Zoom</Button></div>
+        </div>
+        <br/>
+        <div className="Galary1">
+            <div className="Animal5"><Button className="Zoom">Zoom</Button></div>
+            <div className="Animal6"><Button className="Zoom">Zoom</Button></div>
+            <div className="Animal7"><Button className="Zoom">Zoom</Button></div>
+            <div className="Animal8"><Button className="Zoom">Zoom</Button></div>
+        </div>
+        <br/><br/>
+    </div>
+    </div>
+  );
+}

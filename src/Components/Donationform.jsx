0f925322b19a324850447908171d2f6534d72833
ruchika6698 <ruchika6698@gmail.com>
2020-10-19 17:31:51 +0530

    Refactor:Added counter Foundaion Details

--- src/Components/Donationform.jsx
+++ src/Components/Donationform.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import "../SCSS/Donationform.scss";
 import Button from "react-bootstrap/Button";
 
-export default function Donationform() {
+export default class Donationform extends React.Component {
+  state = {
+    transition: "5s all ease-in-out"
+  };
+
+  ScrollEvent = (e) => {
+    if (window.scrollY > 1050) {
+      this.setState({  transition: "0s"});
+    } else {
+      this.setState({  transition: "5s all ease-in-out" });
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.ScrollEvent);
+  }
+
+  render() {
   return (
     <div className="Donationform">
-      <div className="dropdown">
+      <div className="dropdown" style={{ transition: this.state.transition }}>
         <select name="Select An Amount" className="select">
           <option value="Select An Amount">Select An Amount</option>
           <option value="$100" className="option">
@@ -20,14 +37,15 @@ export default function Donationform() {
         </select>
       </div>
       <div>
-        <input type="text" className="name" PlaceHolder="Your Name"></input>
+        <input type="text" className="name" PlaceHolder="Your Name" style={{ transition: this.state.transition }}></input>
       </div>
       <div>
-        <input type="text" className="name" PlaceHolder="Email"></input>
+        <input type="text" className="name" PlaceHolder="Email" style={{ transition: this.state.transition }}></input>
       </div>
       <div>
-        <Button className="button">DONATE NOW</Button>
+        <Button className="button" style={{ transition: this.state.transition }}>DONATE NOW</Button>
       </div>
     </div>
   );
+}
 }
--- src/Components/Foundationdetails.jsx
+++ src/Components/Foundationdetails.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import "../SCSS/Foundationdetails.scss";
+import CountUp from 'react-countup';
 
 export default function Foundationdetails() {
   return (
     <div className="Foundationdetails">
     <div className="Foundationdiv">
         <div className="speciesDiv">
-            <div className="Speciesnumber">50</div>
+            <div className="Speciesnumber"><CountUp end={50} duration={15} /></div>
             <br/>
             <div className="Speciesdetail">SAVED SPECIES</div>
         </div>
         <div className="speciesDiv">
-            <div className="Speciesnumber">92</div>
+            <div className="Speciesnumber"><CountUp end={92} duration={15} /></div>
             <br/>
             <div className="Speciesdetail">TEAM MEMBERS</div>
         </div>
         <div className="speciesDiv">
-            <div className="Speciesnumber">340</div>
+            <div className="Speciesnumber"><CountUp end={340} duration={15} /></div>
             <br/>
             <div className="Speciesdetail">DAILY DONATIONS</div>
         </div>
         <div className="speciesDiv">
-            <div className="Speciesnumber">4</div>
+            <div className="Speciesnumber"><CountUp end={4} duration={15} /></div>
             <br/>
             <div className="Speciesdetail">PARTNERS</div>
         </div>

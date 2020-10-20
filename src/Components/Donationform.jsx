c221787b00721a552b31dd6cbd1ac2c21fc06f39
ruchika6698 <ruchika6698@gmail.com>
2020-10-20 18:27:36 +0530

    Refactor:Added Animation in Our project component

--- src/Components/Donationform.jsx
+++ src/Components/Donationform.jsx
@@ -4,48 +4,80 @@ import Button from "react-bootstrap/Button";
 
 export default class Donationform extends React.Component {
   state = {
-    transition: "5s all ease-in-out"
+    opacity: "0",
+    transition: "3s all ease-in-out",
   };
 
-  ScrollEvent = (e) => {
-    if (window.scrollY > 1050) {
-      this.setState({  transition: "0s"});
+  listenScrollEvent = (e) => {
+    if (window.scrollY > 200) {
+      this.setState({ opacity: "1", transition: "3s all ease-in-out" });
     } else {
-      this.setState({  transition: "5s all ease-in-out" });
+      this.setState({ opacity: "0" });
     }
   };
 
   componentDidMount() {
-    window.addEventListener("scroll", this.ScrollEvent);
+    window.addEventListener("scroll", this.listenScrollEvent);
   }
 
   render() {
-  return (
-    <div className="Donationform">
-      <div className="dropdown" style={{ transition: this.state.transition }}>
-        <select name="Select An Amount" className="select">
-          <option value="Select An Amount">Select An Amount</option>
-          <option value="$100" className="option">
-            $100
-          </option>
-          <option value="$200" className="option">
-            $200
-          </option>
-          <option value="Other" className="option">
-            Other
-          </option>
-        </select>
+    return (
+      <div className="Donationform">
+        <div
+          className="dropdown"
+          style={{
+            opacity: this.state.opacity,
+            transition: this.state.transition,
+          }}
+          elevation={0}
+        >
+          <select name="Select An Amount" className="select">
+            <option value="Select An Amount">Select An Amount</option>
+            <option value="$100" className="option">
+              $100
+            </option>
+            <option value="$200" className="option">
+              $200
+            </option>
+            <option value="Other" className="option">
+              Other
+            </option>
+          </select>
+        </div>
+        <div>
+          <input
+            type="text"
+            className="name"
+            PlaceHolder="Your Name"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="text"
+            className="name"
+            PlaceHolder="Email"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+          ></input>
+        </div>
+        <div>
+          <Button
+            className="button"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+          >
+            DONATE NOW
+          </Button>
+        </div>
       </div>
-      <div>
-        <input type="text" className="name" PlaceHolder="Your Name" style={{ transition: this.state.transition }}></input>
-      </div>
-      <div>
-        <input type="text" className="name" PlaceHolder="Email" style={{ transition: this.state.transition }}></input>
-      </div>
-      <div>
-        <Button className="button" style={{ transition: this.state.transition }}>DONATE NOW</Button>
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
--- src/Components/Ourprojects.jsx
+++ src/Components/Ourprojects.jsx
@@ -2,61 +2,140 @@ import React from "react";
 import "../SCSS/Ourprojects.scss";
 import Button from "react-bootstrap/Button";
 
-export default function Ourprojects() {
-  return (
-    <div className="Ourprojects">
-      <div>
-        <div className="projectText">OUR PROJECTS</div>
-        <div className="Message">Help Animals Worldwide</div>
-        <br/>
-        <div className="Textinfo">Below are some of our recent charity projects that helped us</div>
-        <div className="Textinfo">protect wild cat species and save them from from retaliatory</div>
-        <div className="Textinfo">killings in 28 countries.</div>
-      </div>
-      <br/>
-      <br/>
-      <div className="Imagediv">
-      <div>
-        <div className="LionImage">
-            <div className="DonationButtons"><Button className="Donatebutton">DONATE</Button></div>
-        </div>
-        <br/>
-        <div className="Animalname">
-            LIONS
-        </div>
-        <br/>
-        <div className="amount">
-            $340K DONATED
-        </div>
-     </div>
-     <div>
-        <div className="CheetahsImage">
-            <div className="DonationButtons"><Button className="Donatebutton">DONATE</Button></div>
-        </div>
-        <br/>
-        <div className="Animalname">
-            CHEETAHS
-        </div>
-        <br/>
-        <div className="amount">
-            $550K DONATED
-        </div>
-    </div>
+export default class Ourprojects extends React.Component {
+  state = {
+    opacity: "0",
+    transition: "3s all ease-in-out",
+  };
 
-    <div>
-        <div className="CubsImage">
-            <div className="DonationButtons"><Button className="Donatebutton">DONATE</Button></div>
+  listenScrollEvent = (e) => {
+    if (window.scrollY > 400) {
+      this.setState({ opacity: "1", transition: "3s all ease-in-out" });
+    } else {
+      this.setState({ opacity: "0" });
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.listenScrollEvent);
+  }
+
+  render() {
+    return (
+      <div className="Ourprojects">
+        <div>
+          <div
+            className="projectText"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+            elevation={0}
+          >
+            OUR PROJECTS
+          </div>
+          <div
+            className="Message"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+            elevation={0}
+          >
+            Help Animals Worldwide
+          </div>
+          <br />
+          <div
+            className="Textinfo"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+            elevation={0}
+          >
+            Below are some of our recent charity projects that helped us
+          </div>
+          <div
+            className="Textinfo"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+            elevation={0}
+          >
+            protect wild cat species and save them from from retaliatory
+          </div>
+          <div
+            className="Textinfo"
+            style={{
+              opacity: this.state.opacity,
+              transition: this.state.transition,
+            }}
+            elevation={0}
+          >
+            killings in 28 countries.
+          </div>
         </div>
-        <br/>
-        <div className="Animalname">
-            TIGER CUBS
-        </div>
-        <br/>
-        <div className="amount">
-           $610K DONATED
+        <br />
+        <br />
+        <div className="Imagediv">
+          <div>
+            <div
+              className="LionImage"
+              style={{
+                opacity: this.state.opacity,
+                transition: this.state.transition,
+              }}
+              elevation={0}
+            >
+              <div className="DonationButtons">
+                <Button className="Donatebutton">DONATE</Button>
+              </div>
+            </div>
+            <br />
+            <div className="Animalname">LIONS</div>
+            <br />
+            <div className="amount">$340K DONATED</div>
+          </div>
+          <div>
+            <div
+              className="CheetahsImage"
+              style={{
+                opacity: this.state.opacity,
+                transition: this.state.transition,
+              }}
+              elevation={0}
+            >
+              <div className="DonationButtons">
+                <Button className="Donatebutton">DONATE</Button>
+              </div>
+            </div>
+            <br />
+            <div className="Animalname">CHEETAHS</div>
+            <br />
+            <div className="amount">$550K DONATED</div>
+          </div>
+
+          <div>
+            <div
+              className="CubsImage"
+              style={{
+                opacity: this.state.opacity,
+                transition: this.state.transition,
+              }}
+              elevation={0}
+            >
+              <div className="DonationButtons">
+                <Button className="Donatebutton">DONATE</Button>
+              </div>
+            </div>
+            <br />
+            <div className="Animalname">TIGER CUBS</div>
+            <br />
+            <div className="amount">$610K DONATED</div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
-  );
+    );
+  }
 }
